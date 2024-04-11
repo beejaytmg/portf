028fd49732be366d0a4b9e11989fef9f0078d0e3
bijay <tmgbijay869@gmail.com>
2024-04-11 15:00:19 +0545

    Your descriptive commit message here

--- api/cron.js
+++ api/cron.js
@@ -1,5 +1,12 @@
-export default function handler(req, res) {
-  // Run the generateSitemap.js script
-  require('../generateSitemap.js');
-  res.status(200).end('Sitemap generation triggered');
+import { generateSitemap } from '../generateSitemap.js';
+
+export default async function handler(req, res) {
+  try {
+    // Run the generateSitemap function
+    await generateSitemap();
+    res.status(200).end('Sitemap generation triggered');
+  } catch (error) {
+    console.error('Error generating sitemap:', error);
+    res.status(500).end('Error generating sitemap');
+  }
 }
--- generateSitemap.js
+++ generateSitemap.js
@@ -2,28 +2,30 @@ import fs from 'fs';
 import fetch from 'node-fetch';
 import xmlbuilder from 'xmlbuilder';
 
-// Fetch blogs from the API
-fetch('https://authapiko.pythonanywhere.com/blogs')
-    .then(response => response.json())
-    .then(blogs => {
-        const root = xmlbuilder.create('urlset', { encoding: 'utf-8' })
-            .att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+export async function generateSitemap() {
+  try {
+    // Fetch blogs from the API
+    const response = await fetch('https://authapiko.pythonanywhere.com/blogs');
+    const blogs = await response.json();
 
-        blogs.forEach(blog => {
-            root.ele('url')
-                .ele('loc', `https://bijayakumartamang.com.np/blog/${blog.title.replace(/\s+/g, '-').toLowerCase()}/${blog.id}`)
-                .up()
-                .ele('changefreq', 'weekly')
-                .up()
-                .ele('priority', '0.8');
-        });
+    const root = xmlbuilder.create('urlset', { encoding: 'utf-8' })
+      .att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
 
-        const xml = root.end({ pretty: true });
-
-        // Write the XML to sitemap.xml file
-        fs.writeFileSync('public/sitemap.xml', xml, 'utf8');
-        console.log('Sitemap generated successfully!');
-    })
-    .catch(error => {
-        console.error('Error fetching blogs:', error);
+    blogs.forEach((blog) => {
+      root.ele('url')
+        .ele('loc', `https://bijayakumartamang.com.np/blog/${blog.title.replace(/\s+/g, '-').toLowerCase()}/${blog.id}`)
+        .up()
+        .ele('changefreq', 'weekly')
+        .up()
+        .ele('priority', '0.8');
     });
+
+    const xml = root.end({ pretty: true });
+
+    // Write the XML to sitemap.xml file
+    await fs.promises.writeFile('public/sitemap.xml', xml, 'utf8');
+    console.log('Sitemap generated successfully!');
+  } catch (error) {
+    console.error('Error fetching blogs:', error);
+  }
+}
