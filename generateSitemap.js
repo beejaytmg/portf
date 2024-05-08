d52dcdb4313759964f249d83cc95e98f1da551c7
bijay <tmgbijay869@gmail.com>
2024-05-08 10:58:33 +0545

    Your descriptive commit message here

--- generateSitemap.js
+++ generateSitemap.js
@@ -4,26 +4,31 @@ import xmlbuilder from 'xmlbuilder';
 
 // Fetch blogs from the API
 fetch('https://authapiko.pythonanywhere.com/blogs')
-    .then(response => response.json())
-    .then(blogs => {
-        const root = xmlbuilder.create('urlset', { encoding: 'utf-8' })
-            .att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+  .then(response => response.json())
+  .then(blogs => {
+    const root = xmlbuilder.create('urlset', { encoding: 'utf-8' })
+      .att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
 
-        blogs.forEach(blog => {
-            root.ele('url')
-                .ele('loc', `https://bijayakumartamang.com.np/blog/${blog.title.replace(/\s+/g, '-').toLowerCase()}/${blog.id}`)
-                .up()
-                .ele('changefreq', 'weekly')
-                .up()
-                .ele('priority', '0.8');
-        });
+    blogs.forEach(blog => {
+      const lastModifiedDate = new Date(blog.date_published);
+      const lastModifiedISOString = lastModifiedDate.toISOString();
 
-        const xml = root.end({ pretty: true });
-
-        // Write the XML to sitemap.xml file
-        fs.writeFileSync('public/sitemap.xml', xml, 'utf8');
-        console.log('Sitemap generated successfully!');
-    })
-    .catch(error => {
-        console.error('Error fetching blogs:', error);
+      root.ele('url')
+        .ele('loc', `https://bijayakumartamang.com.np/blog/${blog.title.replace(/\s+/g, '-').toLowerCase()}/${blog.id}`)
+        .up()
+        .ele('lastmod', lastModifiedISOString)
+        .up()
+        .ele('changefreq', 'weekly')
+        .up()
+        .ele('priority', '0.8');
     });
+
+    const xml = root.end({ pretty: true });
+
+    // Write the XML to sitemap.xml file
+    fs.writeFileSync('public/sitemap.xml', xml, 'utf8');
+    console.log('Sitemap generated successfully!');
+  })
+  .catch(error => {
+    console.error('Error fetching blogs:', error);
+  });
